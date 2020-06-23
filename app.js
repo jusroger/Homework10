015aae7ec82caccc4ff2a243817cda438d9748b2
Justin Roger <justin@Justins-MacBook-Pro.local>
2020-06-23 13:34:46 -0400

    Team Profile App

---
+++ app.js
@@ -0,0 +1,152 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const render = require("./lib/htmlRenderer");
+
+let finalTeamArray = [];
+
+function startingPrompt() {
+    inquirer.prompt([
+        {
+            message: "Welcome to Team Generator",
+            name: "teamname"
+        }
+    ])
+        .then(function(data){
+            const teamName = data.teamname
+            finalTeamArray.push(teamName)
+            addManager();
+        })
+    
+}
+
+function addManager() {
+    inquirer.prompt([
+        {
+            message: "What is your team manager's name?",
+            name: "name"
+        },
+        {
+            message: "What is your team manager's email address?",
+            name: "email"
+        },
+
+        {
+            type: "number",
+            message: "What is your team manager's office number?",
+            name: "officeNumber"
+        },
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = 1
+            const email = data.email
+            const officeNumber = data.officeNumber
+            const teamMember = new Manager(name, id, email, officeNumber)
+            finalTeamArray.push(teamMember)
+            addTeamMembers();
+        });
+
+}
+
+function addTeamMembers() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: ["Yes, add an engineer", "Yes, add an intern", "No, my team is complete"],
+            name: "addMemberData"
+        }
+    ])
+
+        .then(function (data) {
+
+            switch (data.addMemberData) {
+                case "Yes, add an engineer":
+                    addEngineer();
+                    break;
+
+                case "Yes, add an intern":
+                    addIntern();
+                    break;
+                case "No, my team is complete":
+                    generateTeam();
+                    break;
+            }
+        });
+}
+
+function addEngineer() {
+    inquirer.prompt([
+        {
+            message: "What is this engineer's name?",
+            name: "name"
+        },
+        {
+            message: "What is this engineer's email address?",
+            name: "email"
+        },
+        {
+            message: "What is this engineer's Github profile?",
+            name: "github"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = finalTeamArray.length + 1
+            const email = data.email
+            const github = data.github
+            const teamMember = new Engineer(name, id, email, github)
+            finalTeamArray.push(teamMember)
+            addTeamMembers()
+        });
+
+};
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            message: "What is this intern's name?",
+            name: "name"
+        },
+        {
+            message: "What is this intern's email address?",
+            name: "email"
+        },
+        {
+            message: "What is this intern's school?",
+            name: "school"
+        }
+    ])
+
+        .then(function (data) {
+            const name = data.name
+            const id = finalTeamArray.length + 1
+            const email = data.email
+            const school = data.school
+            const teamMember = new Intern(name, id, email, school)
+            finalTeamArray.push(teamMember)
+            addTeamMembers()
+        });
+
+};
+
+function generateTeam() { 
+    const renderedHTML = render(finalTeamArray);
+    console.log(renderedHTML);
+    htmlWriter(renderedHTML);
+}
+
+function htmlWriter(html) {
+    const OUTPUT_DIR = path.resolve(__dirname, "output");
+    const outputPath = path.join(OUTPUT_DIR, "team.html");
+    fs.writeFileSync (outputPath,html)
+}
+
+startingPrompt()
